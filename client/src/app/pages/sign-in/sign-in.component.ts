0f44d4ef289987ba33a7d71615cbba4bd4f8530f
IvanDeresh <dereshivan0@gmail.com>
2025-04-22 14:41:16 +0200

    "sign-in and sign-up forms implemented"

--- client/src/app/pages/sign-in/sign-in.component.ts
+++ client/src/app/pages/sign-in/sign-in.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './sign-in.component.html',
-  styleUrl: './sign-in.component.scss',
+  styleUrls: ['./sign-in.component.scss'],
 })
-export class SignInComponent {}
+export class SignInComponent {
+  signInForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.signInForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSignIn() {
+    if (this.signInForm.valid) {
+      const { email, password } = this.signInForm.value;
+      console.log('User signed in with:', email, password);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+  goToSignUp() {
+    this.router.navigate(['/sign-up']);
+  }
+}
--- client/src/app/pages/sign-up/sign-up.component.ts
+++ client/src/app/pages/sign-up/sign-up.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './sign-up.component.html',
-  styleUrl: './sign-up.component.scss',
+  styleUrls: ['./sign-up.component.scss'],
 })
-export class SignUpComponent {}
+export class SignUpComponent {
+  signUpForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.signUpForm = this.fb.group({
+      name: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(32),
+        ],
+      ],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSignIn() {
+    if (this.signUpForm.valid) {
+      const { name, email, password } = this.signUpForm.value;
+      console.log('User signed up with:', name, email, password);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+  goToSignIn() {
+    this.router.navigate(['/sign-in']);
+  }
+}
